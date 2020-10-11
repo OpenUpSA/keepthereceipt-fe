74782055df04d57ea4851dc2aaaf180a088d3983
JD Bothma <jbothma@gmail.com>
2020-10-11 19:01:58 +0200

    Fix empty dropdowns and duplication in dropdows

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -1,18 +1,25 @@
 class DropdownOption {
-  constructor(activeTemplate, inactiveTemplate, selectHandler, deselectHandler, optionItem) {
+  activeTemplate = $(".styles .dropdown-list__active").first();
+  inactiveTemplate = $(".styles .dropdown-list__links").first();
+
+  constructor(selectHandler, deselectHandler, optionItem) {
     if (optionItem.selected) {
-      this.element = activeTemplate.clone();
+      this.element = this.activeTemplate.clone();
       this.element.click((e => {
         deselectHandler(optionItem.label);
         e.preventDefault();
       }).bind(this));
     } else {
-      this.element = inactiveTemplate.clone();
+      this.element = this.inactiveTemplate.clone();
       this.element.click((e => {
         selectHandler(optionItem.label);
         e.preventDefault();
       }).bind(this));
     }
+    console.assert(this.element.length === 1);
+    this.element.find(".receipt-mode").remove();
+    this.element.find(".dropdown-link__text + div").addClass("facet-count").text("");
+
     const optionLabelEl = this.element.find(".dropdown-link__text");
     optionLabelEl.text(optionItem.label);
     optionLabelEl.attr("title", optionItem.label);
@@ -23,16 +30,15 @@ class DropdownOption {
 export class DropdownField {
   constructor(element, label, queryField) {
     this.element = element;
+    console.assert(this.element.length === 1);
+
+    this.itemContainer = this.element.find(".dropdown-list__inner");
+    console.assert(this.itemContainer.length === 1);
+
     this.label = label;
     this.queryField = queryField;
     this.addFilterHandlers = [];
     this.removeFilterHandlers = [];
-    this.activeOptionTemplate = element.find(".dropdown-list__active").clone();
-    this.inactiveOptionTemplate = element.find(".dropdown-list__links").clone();
-    this.activeOptionTemplate.find(".dropdown-link__text").text("");
-    this.activeOptionTemplate.find(".dropdown-link__text + div").addClass("facet-count").text("");
-    this.inactiveOptionTemplate.find(".dropdown-link__text").text("");
-    this.inactiveOptionTemplate.find(".dropdown-link__text + div").addClass("facet-count").text("");
 
     this.reset();
   }
@@ -53,13 +59,11 @@ export class DropdownField {
   updateOptions(options) {
     options.forEach(optionItem => {
       const option = new DropdownOption(
-        this.activeOptionTemplate,
-        this.inactiveOptionTemplate,
         this.handleOptionSelect.bind(this),
         this.handleOptionDeselect.bind(this),
         optionItem
       );
-      this.element.find(".dropdown-list__inner").append(option.element);
+      this.itemContainer.append(option.element);
     });
   }
 
