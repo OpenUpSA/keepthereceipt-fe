9e5a38b8ec246fd7fff0b416d1405bf9aa126d0c
JD Bothma <jbothma@gmail.com>
2020-10-14 08:10:55 +0200

    Present blanks in filters as Blank in italics

--- src/js/components/dropdown.js
+++ src/js/components/dropdown.js
@@ -21,7 +21,12 @@ class DropdownOption {
     this.element.find(".dropdown-link__text + div").addClass("facet-count").text("");
 
     const optionLabelEl = this.element.find(".dropdown-link__text");
-    optionLabelEl.text(optionItem.label);
+    if (optionItem.label) {
+      optionLabelEl.text(optionItem.label);
+    } else {
+      optionLabelEl.text("Blank");
+      optionLabelEl.css('font-style', 'italic');
+    }
     optionLabelEl.attr("title", optionItem.label);
     this.element.find(".facet-count").text(optionItem.count);
   }
