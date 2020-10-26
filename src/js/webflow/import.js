d69302b6b66d80435b8949f870bf4bc577406d86
JD Bothma <jbothma@gmail.com>
2020-10-26 20:23:40 +0200

    Add pum.js for responsive embedding
    Just use polling option right now since I don't kow in js when it's done drawing.

--- src/js/webflow/import.js
+++ src/js/webflow/import.js
@@ -1,6 +1,19 @@
 exports.transform = function(window, $) {
   // Adding a script tag to body via jQuery seems to add it to head as well
-  const tag = window.document.createElement("script");
-  tag.setAttribute("src", "js/index.js");
-  window.document.body.appendChild(tag);
+
+
+  addScriptToBody(window, {src: "https://pym.nprapps.org/pym.v1.min.js"});
+  addScriptToBody(window, {src: "js/index.js"});
 };
+
+function addScriptToBody(window, attrs, text) {
+  // Adding a script tag to body via jQuery seems to add it to head as well
+  const tag = window.document.createElement("script");
+  for (let name in attrs)
+    tag.setAttribute(name, attrs[name]);
+  if (text)
+    tag.appendChild(window.document.createTextNode(text));
+
+  window.document.body.appendChild(tag);
+  window.document.body.appendChild(window.document.createTextNode("\n"));
+}
